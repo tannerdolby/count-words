baf1cc3024284075b58b15c1dce909e041fdaf64
Tanner <tannercdolby@gmail.com>
2022-04-07 21:00:42 -0700

    adds generated files

--- dist/word-counts.d.ts
+++ dist/word-counts.d.ts
@@ -3,44 +3,33 @@ declare type FrequencyObject = {
     frequency: number;
     usage: number;
 };
-interface FrequencyMap {
+declare type FrequencyMap = {
     [key: string]: FrequencyObject;
-}
-/**
- * A class for counting the frequency of words in a given string or file.
- */
-declare class WordCounts {
+};
+interface WordTable {
     frequencies: FrequencyMap;
     wordList: Array<string>;
     uniqueWordList: Array<string>;
     sortedUniqueWordList: Array<string>;
     sortedFrequencyList: Array<FrequencyMap>;
-    wordRegex: RegExp;
     wordCount: number;
     uniqueWordCount: number;
-    constructor();
-    /**
-     * Count the frequency of words in a given string.
-     * @param {string} document A string representing the text content to scan.
-     * @return {Map<string, FrequencyObject>} A hash map sorted in ascending order (a-z) containing all words and their frequencies.
-     */
-    count(doc: string): FrequencyMap;
-    /**
-     * Count the frequency of words in a given local file containing text content.
-     * @param {string} filePath A local filepath representing a document to be scanned.
-     * @param {string} encoding Character encoding to be used for reading the file located at `filePath`. Default is "utf8".
-     * @return {FrequencyMap} A hash table sorted in ascending order (a-z) containing all words and their frequencies.
-     */
-    countWordsInFile(filePath: string, encoding?: string): FrequencyMap;
-    /**
-     * Get the nth word from frequency table as it appears in the document.
-     * @param {string} target The nth word to lookup. (0-indexed)
-     * @return The word object from frequency table representing the nth word.
-     */
-    getNthWord(target: number): FrequencyMap | undefined;
-    printFrequencies(): string;
-    private searchByValue;
-    private sortByFrequency;
-    private hasScanRun;
-    private doesScanDataExist;
+    getNthWord: Function;
+    printFrequencies: Function;
 }
+declare const wordRegex: RegExp;
+/**
+ * Count the frequency of words in a given string.
+ * @param {string} document A string representing the text content to scan.
+ * @return {WordTable} A hash map populated with word count data.
+ */
+declare function countWords(doc: string): WordTable;
+/**
+ * Count the frequency of words in a given local file containing text content.
+ * @param {string} filePath A local filepath representing a document to be scanned.
+ * @param {string} encoding Character encoding to be used for reading the file located at `filePath`. Default is "utf8".
+ * @return {WordTable} A hash map containing word data.
+ */
+declare function countWordsInFile(filePath: string, encoding?: string): WordTable;
+declare function searchByValue(frequencies: FrequencyMap, pair: FrequencyObject): FrequencyMap;
+declare function sortByFrequency(frequencies: FrequencyMap): Array<FrequencyMap>;
--- dist/word-counts.js
+++ dist/word-counts.js
@@ -1,113 +1,89 @@
 "use strict";
 const fs = require("fs");
+;
+const wordRegex = /(\w+\'\w+)|(\w+)/;
 /**
- * A class for counting the frequency of words in a given string or file.
+ * Count the frequency of words in a given string.
+ * @param {string} document A string representing the text content to scan.
+ * @return {WordTable} A hash map populated with word count data.
  */
-class WordCounts {
-    constructor() {
-        this.frequencies = {};
-        this.sortedFrequencyList = [];
-        this.wordList = [];
-        this.uniqueWordList = [];
-        this.sortedUniqueWordList = [];
-        this.wordRegex = /(\w+\'\w+)|(\w+)/;
-        this.wordCount = 0;
-        this.uniqueWordCount = 0;
-    }
-    /**
-     * Count the frequency of words in a given string.
-     * @param {string} document A string representing the text content to scan.
-     * @return {Map<string, FrequencyObject>} A hash map sorted in ascending order (a-z) containing all words and their frequencies.
-     */
-    count(doc) {
-        let words = doc.split(/\s+/).map(word => {
-            const cleanWordObj = word.match(this.wordRegex);
-            const cleanWord = cleanWordObj && cleanWordObj[0] != undefined ? cleanWordObj[0] : "";
-            this.frequencies[cleanWord] = { "frequency": 0, "usage": 0 };
-            return cleanWord;
-        });
-        this.wordCount = words.length;
-        this.uniqueWordList = [...new Set(words)];
-        words = words.sort((a, b) => a.localeCompare(b));
-        words.forEach((word) => {
-            this.wordList.push(word);
-            this.sortedUniqueWordList.push(word);
-            const wordData = this.frequencies[word];
-            wordData.frequency += 1;
-            wordData.usage = parseFloat(((wordData.frequency / this.wordCount) * 100).toFixed(1));
-        });
-        this.sortedUniqueWordList = [...new Set(this.sortedUniqueWordList)];
-        this.uniqueWordCount = this.sortedUniqueWordList.length;
-        this.sortedFrequencyList = this.sortByFrequency();
-        return this.frequencies;
-    }
-    /**
-     * Count the frequency of words in a given local file containing text content.
-     * @param {string} filePath A local filepath representing a document to be scanned.
-     * @param {string} encoding Character encoding to be used for reading the file located at `filePath`. Default is "utf8".
-     * @return {FrequencyMap} A hash table sorted in ascending order (a-z) containing all words and their frequencies.
-     */
-    countWordsInFile(filePath, encoding = "utf8") {
-        let doc = fs.readFileSync(filePath, encoding);
-        return this.count(doc);
-    }
-    ;
-    /**
-     * Get the nth word from frequency table as it appears in the document.
-     * @param {string} target The nth word to lookup. (0-indexed)
-     * @return The word object from frequency table representing the nth word.
-     */
-    getNthWord(target) {
-        if (!this.doesScanDataExist())
-            return {};
-        const key = this.uniqueWordList[target];
-        return this.sortedFrequencyList.find(wordObj => Object.keys(wordObj).includes(key));
-    }
-    printFrequencies() {
-        if (!this.doesScanDataExist())
-            return "";
-        let frequencies = "";
-        for (const key in this.frequencies) {
-            frequencies += `${key}: { frequency: ${this.frequencies[key].frequency}, usage: ${this.frequencies[key].usage} }\n`;
-        }
-        return frequencies.slice(0, frequencies.length - 1);
-    }
-    searchByValue(pair) {
-        let foundWord = {};
-        for (const key in this.frequencies) {
-            if (this.frequencies[key] == pair) {
-                foundWord[key] = pair;
-                return foundWord;
+function countWords(doc) {
+    const wordTable = {
+        frequencies: {},
+        wordList: [],
+        uniqueWordList: [],
+        sortedUniqueWordList: [],
+        sortedFrequencyList: [],
+        wordCount: 0,
+        uniqueWordCount: 0,
+        getNthWord(target) {
+            const key = this.uniqueWordList[target];
+            return this.sortedFrequencyList.find(wordObj => Object.keys(wordObj).includes(key));
+        },
+        printFrequencies() {
+            let frequencyStr = "";
+            for (const key in this.frequencies) {
+                frequencyStr += `${key}: { frequency: ${this.frequencies[key].frequency}, usage: ${this.frequencies[key].usage} }\n`;
             }
+            return frequencyStr.slice(0, frequencyStr.length - 1);
         }
-        return foundWord;
-    }
-    sortByFrequency() {
-        if (!this.doesScanDataExist())
-            return [];
-        let frequencyList = [];
-        for (const key in this.frequencies) {
-            const wordData = {};
-            wordData[key] = this.frequencies[key];
-            frequencyList.push(wordData[key]);
-        }
-        frequencyList.sort((a, b) => {
-            if (a.frequency == b.frequency)
-                return 0;
-            return a.frequency > b.frequency ? -1 : 1;
-        });
-        return frequencyList.map(wordObj => {
-            return this.searchByValue(wordObj);
-        });
-    }
-    hasScanRun() {
-        return Object.keys(this.frequencies).length > 0 ? true : false;
-    }
-    doesScanDataExist() {
-        if (!this.hasScanRun())
-            throw Error("No data available. Make sure to run countWordsIn*() to generate frequency tables.");
-        return true;
-    }
+    };
+    let words = doc.split(/\s+/).map(word => {
+        const cleanWordObj = word.match(wordRegex);
+        const cleanWord = cleanWordObj && cleanWordObj[0] ? cleanWordObj[0] : "";
+        wordTable.frequencies[cleanWord] = { "frequency": 0, "usage": 0 };
+        return cleanWord;
+    });
+    wordTable.wordCount = words.length;
+    wordTable.uniqueWordList = [...new Set(words)];
+    words = words.sort((a, b) => a.localeCompare(b));
+    words.forEach((word) => {
+        wordTable.wordList.push(word);
+        wordTable.sortedUniqueWordList.push(word);
+        const wordData = wordTable.frequencies[word];
+        wordData.frequency += 1;
+        wordData.usage = parseFloat(((wordData.frequency / wordTable.wordCount) * 100).toFixed(1));
+    });
+    wordTable.sortedUniqueWordList = [...new Set(wordTable.sortedUniqueWordList)];
+    wordTable.uniqueWordCount = wordTable.sortedUniqueWordList.length;
+    wordTable.sortedFrequencyList = sortByFrequency(wordTable.frequencies);
+    return wordTable;
+}
+/**
+ * Count the frequency of words in a given local file containing text content.
+ * @param {string} filePath A local filepath representing a document to be scanned.
+ * @param {string} encoding Character encoding to be used for reading the file located at `filePath`. Default is "utf8".
+ * @return {WordTable} A hash map containing word data.
+ */
+function countWordsInFile(filePath, encoding = "utf8") {
+    let doc = fs.readFileSync(filePath, encoding);
+    return countWords(doc);
 }
 ;
-module.exports = WordCounts;
+function searchByValue(frequencies, pair) {
+    const foundWord = {};
+    for (const key in frequencies) {
+        if (frequencies[key] == pair) {
+            foundWord[key] = pair;
+            return foundWord;
+        }
+    }
+    return foundWord;
+}
+function sortByFrequency(frequencies) {
+    const frequencyList = [];
+    for (const key in frequencies) {
+        const wordData = {};
+        wordData[key] = frequencies[key];
+        frequencyList.push(wordData[key]);
+    }
+    frequencyList.sort((a, b) => {
+        if (a.frequency == b.frequency)
+            return 0;
+        return a.frequency > b.frequency ? -1 : 1;
+    });
+    return frequencyList.map(wordObj => {
+        return searchByValue(frequencies, wordObj);
+    });
+}
+module.exports = { countWords, countWordsInFile };
